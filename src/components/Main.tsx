c76fa59b0b4c31b84bff81194f86bda8ea09c940
Abdul Wahab <abdulwahab20070@gmail.com>
2024-10-09 17:49:54 +0500

    Initial commit/Home page completed

---
+++ src/components/Main.tsx
@@ -0,0 +1,96 @@
+import Image from "next/image";
+
+export default function Main() {
+  return (
+    <>
+      <div
+        style={{
+          backgroundImage: `url('/images/Home_BG.png')`,
+        }}
+        className="container flex w-full h-[660px] bg-[url('/public/background.png')] bg-cover bg-center mt-3"
+      >
+        <div className="left w-1/2 mx-1 h-full  flex flex-col">
+          <h1 className="text-8xl font-serif mt-40 ml-16 font-bold bg-gradient-to-r from-fuchsia-700 via-purple-700 to-purple-700 text-transparent bg-clip-text">
+            NIKE
+          </h1>
+          <h1 className="text-6xl ml-16 font-bold bg-gradient-to-r from-fuchsia-700 via-purple-700 to-purple-700 text-transparent bg-clip-text">
+            COLLECTION
+          </h1>
+          <p className="ml-16 mt-6 text-md">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
+            quod, libero aliquam perferendis quae molestias ratione dolorem
+            optio quos mollitia dolore, quaerat esse sint possimus aliquid
+            asperiores itaque officia repellendus.
+          </p>
+          <div className="flex ml-16 mt-4 w-[199px]">
+            <div className="relative group">
+              <button className="relative inline-block p-px font-semibold leading-6 text-black bg-gray-800 cursor-pointer rounded-xl transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95">
+                <span className="absolute inset-0 rounded-xl bg-gradient-to-r from-fuchsia-700 via-purple-700 to-purple-700 p-[2px] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+
+                <span className="relative z-10 block px-6 py-3 rounded-xl bg-white">
+                  <div className="relative z-10 flex items-center space-x-2">
+                    <span className="transition-all duration-500 group-hover:translate-x-1">
+                      SHOP NOW
+                    </span>
+                    <svg
+                      className="w-6 h-6 transition-transform duration-500 group-hover:translate-x-1"
+                      data-slot="icon"
+                      aria-hidden="true"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        clip-rule="evenodd"
+                        d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
+                        fill-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </div>
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className=" flex image w-2/5 mx-1 h-full">
+          <Image
+            src="/images/shoes.png"
+            width={688}
+            height={0}
+            alt="Nike logo"
+          />
+        </div>
+        <div className="right flex flex-col items-end justify-center w-1/12 h-full mx-1">
+          <Image
+            src="/icons/facebook.png"
+            width={40}
+            height={0}
+            alt="Nike logo"
+            className="my-2"
+          />
+          <Image
+            src="/icons/youtube.png"
+            width={40}
+            height={0}
+            alt="Nike logo"
+            className="my-2"
+          />
+          <Image
+            src="/icons/twitter.png"
+            width={40}
+            height={0}
+            alt="Nike logo"
+            className="my-2"
+          />
+          <Image
+            src="/icons/instagram.png"
+            width={40}
+            height={0}
+            alt="Nike logo"
+            className="my-2"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+
+export default function Navbar() {
+  const anchorClass =
+    "m-5 text-xl font-semibold hover:bg-gradient-to-r from-fuchsia-700 to-purple-700 hover:text-transparent hover:bg-clip-text hover:font-bold ";
+  const iconClass =
+    "material-symbols-outlined mx-2 hover:bg-gradient-to-r from-fuchsia-700 to-purple-700 hover:text-transparent hover:bg-clip-text hover:font-bold hover:font-semibold";
+  return (
+    <>
+      <div className="container h-16 w-full flex shadow-md ">
+        <div className="image w-1/4 flex items-center">
+          <h1 className="text-6xl  ml-16 font-bold italic tracking-tighter bg-gradient-to-r from-fuchsia-700 via-purple-700 to-purple-700 text-transparent bg-clip-text">
+            NIKE&nbsp;
+          </h1>
+          {/* <Image
+            src="https://miro.medium.com/v2/resize:fit:828/format:webp/0*1VmmA6pxXSo7NpGv.jpg"
+            width={110}
+            height={0}
+            alt="Nike logo"\s
+            className="ml-16"
+          /> */}
+        </div>
+        <div className="mid w-2/4 flex justify-center items-center">
+          <a className={anchorClass} href="">
+            Home
+          </a>
+          <a className={anchorClass} href="">
+            Products
+          </a>
+          <a className={anchorClass} href="">
+            Reviews
+          </a>
+          <a className={anchorClass} href="">
+            About
+          </a>
+          <a className={anchorClass} href="">
+            Services
+          </a>
+        </div>
+        <div className="right w-1/4 flex items-center justify-center pt-2">
+          <a href="">
+            <span className={iconClass}>favorite</span>
+          </a>
+          <a href="">
+            <span className={iconClass}>person</span>
+          </a>
+          <a href="">
+            <span className={iconClass}>shopping_cart</span>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
